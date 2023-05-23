4d601df03fead0ce23ed0127dc27407361ff8b4c
deepakkapse <41117353+deepakkapse@users.noreply.github.com>
2023-05-23 23:32:29 +0530

    Add files via upload
    Added availbility set to the VM

---
+++ day6/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group" {
+  type    = string
+  default = "1-6e4bece9-playground-sandbox"
+}
+variable "resource_group_location" {
+  type    = string
+  default = "East US"
+
+}
+/*
+variable "storage_accountname" {
+  type        = string
+  description = "enter the storage account name"
+
+}
+*/
+
+variable "admin_name" {
+  type    = string
+  default = "deepak"
+
+}
+
+variable "admin_pass" {
+  type    = string
+  default = "Azure@12345"
+
+}
