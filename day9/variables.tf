41d7084b9b863902d238f216ead36ad1242e7dec
deepakkapse <41117353+deepakkapse@users.noreply.github.com>
2023-05-25 20:25:27 +0530

    Add files via upload
    Cloud init and sshkey gen in linux

---
+++ day9/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group" {
+  type    = string
+  default = "1-bd5750e3-playground-sandbox"
+}
+variable "resource_group_location" {
+  type    = string
+  default = "South Central US"
+
+}
+/*
+variable "storage_accountname" {
+  type        = string
+  description = "enter the storage account name"
+
+}
+*/
+
+variable "admin_name" {
+  type    = string
+  default = "deepak"
+
+}
+
+variable "admin_pass" {
+  type    = string
+  default = "Azure@12345"
+
+}
